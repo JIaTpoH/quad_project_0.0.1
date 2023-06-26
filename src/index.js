dac00c4590fd80a6b1c1af7b1db4622c3e56a92b
Dmitri Mandaji <badmopss@gmail.com>
2023-06-26 17:55:22 +0200

    add a Routes, Home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+
+import './styles/index.css';
 
 import App from './components/App/App';
 
-createRoot (document.getElementById("root")).render(<App/>);
+createRoot (document.getElementById("root")).render(<BrowserRouter> <App/> </BrowserRouter>);
---
+++ src/utils/routes.js
@@ -0,0 +1,3 @@
+import const Routes = {
+  Home: 
+}
