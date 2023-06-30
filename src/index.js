dedae9f9bb3b859d097c77f2862657c9ee549aed
Dmitri Mandaji <badmopss@gmail.com>
2023-06-30 17:53:11 +0200

    repair json

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import { HashRouter } from 'react-router-dom';
+// import { Provider } from "react-redux";
 
 import "./styles/index.css";
 
-import { store } from "./features/store";
+// import { store } from "./features/store";
 
 import App from "./components/App/App";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
+  
+    <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </Provider>
+    </HashRouter>
+  
+  </React.StrictMode>
 );
